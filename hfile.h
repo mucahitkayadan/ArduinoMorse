aebc24d48ceb459bf50ac1f9a78a6778a33b9e50
Mucahit Kayadan <muhammetkayadan@hotmail.com>
2023-06-06 21:38:12 -0500

    Update hfile.h

--- hfile.h
+++ hfile.h
@@ -1,18 +1,19 @@
 #ifndef HFILE_H_INCLUDED
 #define HFILE_H_INCLUDED
+
 #include "Arduino.h"
 
 class Morse
 {
-  public:
-    Morse(int pin);
-    void dot(int num);
-    void dash(int num);
-    void dot();
-    void dash();
-  private:
-    int _pin;
-};
+public:
+    Morse(int pin);  // Constructor
+    void dot();      // Emit a dot
+    void dash();     // Emit a dash
+    void dot(int num);   // Emit multiple dots
+    void dash(int num);  // Emit multiple dashes
 
+private:
+    int _pin;   // The pin number for Morse code output
+};
 
 #endif // HFILE_H_INCLUDED

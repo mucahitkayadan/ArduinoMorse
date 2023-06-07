49a29c0dd2e5ccbb4b01e320b653a88a73786ade
Mucahit Kayadan <muhammetkayadan@hotmail.com>
2023-06-06 21:38:35 -0500

    Update sfile.cpp

--- sfile.cpp
+++ sfile.cpp
@@ -3,39 +3,44 @@
 
 Morse::Morse(int pin)
 {
-  pinMode(pin, OUTPUT);
-  _pin = pin;
+    pinMode(pin, OUTPUT);   // Set the specified pin as OUTPUT
+    _pin = pin;             // Store the pin number in the private member variable
 }
 
 void Morse::dot()
 {
-  digitalWrite(_pin, HIGH);
-  delay(250);
-  digitalWrite(_pin, LOW);
-  delay(250);
+    digitalWrite(_pin, HIGH);   // Turn on the LED or activate the output
+    delay(250);                  // Delay for dot duration
+    digitalWrite(_pin, LOW);    // Turn off the LED or deactivate the output
+    delay(250);                  // Delay between dots
 }
 
 void Morse::dash()
 {
-  digitalWrite(_pin, HIGH);
-  delay(1000);
-  digitalWrite(_pin, LOW);
-  delay(250);
+    digitalWrite(_pin, HIGH);   // Turn on the LED or activate the output
+    delay(1000);                 // Delay for dash duration
+    digitalWrite(_pin, LOW);    // Turn off the LED or deactivate the output
+    delay(250);                  // Delay between dashes
 }
 
-void Morse::dot(int num){
-    for(int i = 0 ; i < num ; i++){
-        digitalWrite(_pin, HIGH);
-        delay(250);
-        digitalWrite(_pin, LOW);
-        delay(250);
+void Morse::dot(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        digitalWrite(_pin, HIGH);   // Turn on the LED or activate the output
+        delay(250);                  // Delay for dot duration
+        digitalWrite(_pin, LOW);    // Turn off the LED or deactivate the output
+        delay(250);                  // Delay between dots
     }
 }
-void Morse::dash(int num){
-    for(int i = 0 ; i < num ; i++){
-        digitalWrite(_pin, HIGH);
-        delay(1000);
-        digitalWrite(_pin, LOW);
-        delay(250);
+
+void Morse::dash(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        digitalWrite(_pin, HIGH);   // Turn on the LED or activate the output
+        delay(1000);                 // Delay for dash duration
+        digitalWrite(_pin, LOW);    // Turn off the LED or deactivate the output
+        delay(250);                  // Delay between dashes
     }
 }
